f99bc6660e83df1122de096bed4310771003a29d
adikur31 <adi256980@gmail.com>
2019-02-05 16:20:04 +0700

    kelompok B ,
    mebuat weather app react..

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1>..................................</h1>
+		<h1 className="title-container__title">Mencari Cuaca Di Seluruh Dunia </h1>
+		<h2 className="title-container__subtitle">Menemukan temperature dll</h2>
+		
+		<h2 className="title-container__subtitle">request API</h2>
+	</div>
+);
+
+export default Titles;
